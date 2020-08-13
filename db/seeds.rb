40cae5be0128770882e2d02b317161748d022927
Boran Zaza <boran.m.zaza@gmail.com>
2020-08-13 14:47:20 -0400

    Created controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,8 @@
 #
 # Examples:
 #
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
